8bb5eff6378f5a54fde0103b3c0dbd01dde2298e
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2024-11-30 12:29:41 +0000

    Setup lidar ld06

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -e
 vcs import < src/ros2.repos src
 sudo apt-get update
 rosdep update --rosdistro=$ROS_DISTRO
-rosdep install --from-paths src --ignore-src -y --rosdistro=$ROS_DISTRO
+rosdep install --from-paths src --ignore-src -r -y --rosdistro=$ROS_DISTRO
