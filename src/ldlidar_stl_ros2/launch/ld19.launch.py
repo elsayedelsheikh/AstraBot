8f562e333ea501418ae1215703755a0d9f97789c
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2024-12-02 11:54:34 +0000

    Add modified lidar driver

--- src/ldlidar_stl_ros2/launch/ld19.launch.py
+++ src/ldlidar_stl_ros2/launch/ld19.launch.py
@@ -26,14 +26,13 @@ def generate_launch_description():
   ldlidar_node = Node(
       package='ldlidar_stl_ros2',
       executable='ldlidar_stl_ros2_node',
-      name='LD06',
+      name='LD19',
       output='screen',
       parameters=[
-        {'product_name': 'LDLiDAR_LD06'},
+        {'product_name': 'LDLiDAR_LD19'},
         {'topic_name': 'scan'},
         {'frame_id': 'base_laser'},
-        {'port_name': '/dev/ttyTHS1'},
-        {'bins': 454},
+        {'port_name': '/dev/ttyUSB0'},
         {'port_baudrate': 230400},
         {'laser_scan_dir': True},
         {'enable_angle_crop_func': False},
@@ -46,8 +45,8 @@ def generate_launch_description():
   base_link_to_laser_tf_node = Node(
     package='tf2_ros',
     executable='static_transform_publisher',
-    name='base_link_to_base_laser_ld06',
-    arguments=['0.06','0.06','0.15','0','0','0','base_link','base_laser']
+    name='base_link_to_base_laser_ld19',
+    arguments=['0','0','0.18','0','0','0','base_link','base_laser']
   )
 
 
--- src/ldlidar_stl_ros2/launch/stl27l.launch.py
+++ src/ldlidar_stl_ros2/launch/stl27l.launch.py
@@ -26,19 +26,18 @@ def generate_launch_description():
   ldlidar_node = Node(
       package='ldlidar_stl_ros2',
       executable='ldlidar_stl_ros2_node',
-      name='LD06',
+      name='STL27L',
       output='screen',
       parameters=[
-        {'product_name': 'LDLiDAR_LD06'},
+        {'product_name': 'LDLiDAR_STL27L'},
         {'topic_name': 'scan'},
         {'frame_id': 'base_laser'},
-        {'port_name': '/dev/ttyTHS1'},
-        {'bins': 454},
-        {'port_baudrate': 230400},
-        {'laser_scan_dir': True},
+        {'port_name': '/dev/ttyUSB0'},
+        {'port_baudrate': 921600},
+        {'laser_scan_dir': False},
         {'enable_angle_crop_func': False},
-        {'angle_crop_min': 135.0},
-        {'angle_crop_max': 225.0}
+        {'angle_crop_min': 0.0},
+        {'angle_crop_max': 0.0}
       ]
   )
 
@@ -46,8 +45,8 @@ def generate_launch_description():
   base_link_to_laser_tf_node = Node(
     package='tf2_ros',
     executable='static_transform_publisher',
-    name='base_link_to_base_laser_ld06',
-    arguments=['0.06','0.06','0.15','0','0','0','base_link','base_laser']
+    name='base_link_to_base_laser_stl27l',
+    arguments=['0','0','0.18','0','0','0','base_link','base_laser']
   )
 
 
